19f14581729f09d1762f924681fd6a2ed4d55e94
T-Brick <thea@theabrick.com>
2023-02-11 01:49:11 -0500

    2 address assembly

--- C0deine/AbsAsm/AbsAsm2.lean
+++ C0deine/AbsAsm/AbsAsm2.lean
@@ -11,11 +11,22 @@ inductive Operand
 | temp (t : Temp)
 | reg (r : Register)
 
+def Operand.toString : Operand → String
+  | const c => s!"${c}"
+  | mem m => s!"m{m}"
+  | temp t => s!"{t}"
+  | reg r => s!"{r}"
+instance : ToString Operand where toString := Operand.toString
+
 structure Addr where
   base : Temp
   offset : Int
   index : Option Temp
   scale : Int
+
+instance : ToString Addr where
+  toString | addr => s!"{addr.base}, {addr.offset}, {addr.index} {addr.scale}"
+
 
 inductive Instr
 | label (lbl : Label)
@@ -31,7 +42,7 @@ inductive Instr
 | or (src dest : Operand)
 | sal (src dest : Operand)
 | sar (src dest : Operand)
-| cmp (cmp : Comparator) (lhs rhs : Operand)
+| cmp (c : Comparator) (lhs rhs : Operand)
 | jump (lbl : Label)
 | cjump (lbl : Label)
 | call (lbl : Label)
@@ -41,3 +52,28 @@ inductive Instr
 | null_check (check : Operand)
 | shift_check (check : Operand)
 | bounds_check (src : Temp) (index : Operand)
+
+def Instr.toString : Instr → String
+  | label (lbl : Label) => s!"{lbl}:"
+  | directive (str : String) => s!".{str}"
+  | comment (str : String) => s!"#{str}"
+  | mov (src : Operand) (dest : Operand) => s!"\tmov\t\t{src}, {dest}"
+  | add (src : Operand) (dest : Operand) => s!"\tadd\t\t{src}, {dest}"
+  | sub (src : Operand) (dest : Operand) => s!"\tsub\t\t{src}, {dest}"
+  | mul (src : Operand) => s!"\tmul\t\t{src}"
+  | divmod (src : Operand) => s!"\tdivmod\t\t{src}"
+  | and (src : Operand) (dest : Operand) => s!"\tand\t\t{src}, {dest}"
+  | xor (src : Operand) (dest : Operand) => s!"\txor\t\t{src}, {dest}"
+  | or (src : Operand) (dest : Operand) => s!"\tor\t\t{src}, {dest}"
+  | sal (src : Operand) (dest : Operand) => s!"\tsal\t\t{src}, {dest}"
+  | sar (src : Operand) (dest : Operand) => s!"\tsar\t\t{src}, {dest}"
+  | cmp (c : Comparator) (lhs : Operand) (rhs : Operand) => s!"\tcmp\t\t{lhs} {c} {rhs}"
+  | jump (lbl : Label) => s!"\tjump\t\t{lbl}"
+  | cjump (lbl : Label) => s!"\tcjump\t\t{lbl}"
+  | call (lbl : Label) => s!"\tcall\t\t{lbl}"
+  | ret => s!"\tret"
+  | load (src : Addr) (dest : Operand) => s!"\tload\t\t{src}, {dest}"
+  | store (src : Operand) (dest : Addr) => s!"\tstore\t\t{src}, {dest}"
+  | null_check (check : Operand) => s!"\tnull?\t\t{check}"
+  | shift_check (check : Operand) => s!"\tshift?\t\t{check}"
+  | bounds_check (src : Temp) (index : Operand) => s!"\tbounds?\t\t{src}[{index}]"
