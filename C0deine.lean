01dc93c78a6654cac8e65bdcdb307bf69acaec54
James <jamesgallicchio@gmail.com>
2023-03-19 03:09:40 -0400

    add files to C0deine.lean

--- C0deine.lean
+++ C0deine.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+import C0deine.AbsAsm.AbsAsm2
+import C0deine.IrTree.IrTree
+import C0deine.Parser.Abstractor
+import C0deine.Parser.Ast
+import C0deine.Parser.Basic
+import C0deine.Parser.Cst
+import C0deine.Target.Wasm
+import C0deine.Target.X86
+import C0deine.Type.Tst
+import C0deine.Type.Typ
+import C0deine.Type.Typechecker
+import C0deine.Utils.Comparison
+import C0deine.Utils.Context
+import C0deine.Utils.Label
+import C0deine.Utils.Language
+import C0deine.Utils.Register
+import C0deine.Utils.Symbol
+import C0deine.Utils.Temp
