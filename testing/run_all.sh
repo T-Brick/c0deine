8d1a288c697ce62855cbc9375d617307ff7c0d49
T-Brick <thea@theabrick.com>
2024-01-14 01:51:53 -0500

    add clean and infloop to runall

--- testing/run_all.sh
+++ testing/run_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sh ./clean.sh
 sh ./run.sh tests/l4/
 sh ./run.sh tests/c0/
 sh ./run.sh tests/bugs/
 # sh ./run.sh tests/stdlib/conio
 sh ./run.sh tests/stdlib/parse
 sh ./run.sh tests/stdlib/string
+sh ./run.sh tests/infloop
